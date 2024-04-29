b57a237b305029a4e7c76914d9e727a7aa74ad50
glauco <glauquinho2@gmail.com>
2024-04-29 20:01:36 -0300

    commit controle de fluxo

---
+++ controle-de-fluxo/src/CaixaEletronico.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class CaixaEletronico {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        double saldo = 25.0;
+
+        System.out.print("Digite o valor de saque: ");
+        Double valorSolicitado = scanner.nextDouble();
+
+        if(valorSolicitado < saldo){
+
+            saldo = saldo - valorSolicitado;
+            System.out.println("Tudo certo, aguarde a contagem...");
+
+            System.out.println("novo saldo:" + saldo);
+
+        }
+
+        else{
+
+            System.out.println("Saldo insuficiente para saque.");
+
+        }
+
+    }
+}
