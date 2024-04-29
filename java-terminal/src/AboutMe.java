cfedaba3e9fb7ccc163783c54a3dd9cfad246370
glauco <glauquinho2@gmail.com>
2024-04-28 23:11:21 -0300

    ultimo commit do dia 28-04-2024 sobre java

--- java-terminal/src/AboutMe.java
+++ java-terminal/src/AboutMe.java
@@ -1,3 +1,29 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AboutMe {
-    
+    public static void main(String[] args) {
+        //criand o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.print("Digite seu nome:");
+        String nome = scanner.next();
+
+        System.out.print("Digite seu sobrenome:");
+        String sobrenome = scanner.next();
+
+        System.out.print("Digite sua idade:");
+        int idade = scanner.nextInt();
+
+        System.out.print("Digite sua altura:");
+        double altura = scanner.nextDouble();
+
+        System.out.println("");
+
+        //imprimindo os dados obtidos pelo usuario
+        System.out.println("Olá, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos");
+        System.out.println("Minha altura é " + altura + "cm ");
+
+    }
 }
